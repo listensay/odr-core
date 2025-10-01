fee555e91627fda0fe94c017689634f6d2af9397
codemmk <2107228359@qq.com>
2025-10-01 09:46:20 +0800

    feat: install swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Odr-core API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('odr-core-api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 8080);
+  console.log('项目端口成功运行：http://localhost:8080/');
+  console.log('Swagger: http://localhost:8080/odr-core-api');
 }
 bootstrap();
